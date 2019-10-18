129fb466f634a45f8e3433dbf5b9efcec1a61e40
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-18 10:29:56 +0100

    refactoring add Color component -wip

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -11,12 +11,14 @@ const initialColor = {
 const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
+  const [addingColor, setaddingColor] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
+
 
   const saveEdit = e => {
     e.preventDefault();
@@ -102,7 +104,38 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
+
       <div className="spacer" />
+      {/* {
+  <form onSubmit={saveEdit}>
+          <legend>edit color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorToEdit({ ...colorToEdit, color: e.target.value })
+              }
+              value={colorToEdit.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorToEdit({
+                  ...colorToEdit,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToEdit.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit">save</button>
+            <button onClick={() => setEditing(false)}>cancel</button>
+          </div>
+        </form>
+      } */}
       {/* stretch - build another form here to add a color */}
      
     </div>

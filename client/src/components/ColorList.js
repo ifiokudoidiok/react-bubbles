81cfe64cb312306ec497e83810ef1bd5936261ea
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-18 09:56:22 +0100

    delete and edite functionality implemented

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../axios";
 
 const initialColor = {
   color: "",
@@ -21,10 +21,32 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(response => {
+        const editedColor = response.data;
+        updateColors(
+          colors.map(color => {
+            if (color.id === editedColor.id) return editedColor;
+            return color;
+          })
+        );
+        setColorToEdit(initialColor);
+        setEditing(false);
+      })
+      .catch(err => console.log(err));
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(({ data }) => {
+        console.log(data);
+        console.log(colors);
+
+        updateColors(colors.filter(color => color.id !== data));
+      })
+      .catch(err => console.log(err));
   };
 
   return (

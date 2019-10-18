f7751533fcb24e2073be66e8786b06f92ac7d5e2
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-18 10:59:06 +0100

    styled AddColor Component

--- client/src/components/AddColor.js
+++ client/src/components/AddColor.js
@@ -1,20 +1,20 @@
-import React, { useRef } from 'react';
-import axiosWithAuth from '../axios';
+import React, { useRef } from "react";
+import axiosWithAuth from "../axios";
 
 export default function AddColor(props) {
   const colorNameRef = useRef();
   const colorHexRef = useRef();
- 
 
   const submit = () => {
-    axiosWithAuth().post('http://localhost:5000/api/colors/', {
+    axiosWithAuth()
+      .post("http://localhost:5000/api/colors/", {
         color: colorNameRef.current.value,
         code: {
-            hex: colorHexRef.current.value,
+          hex: colorHexRef.current.value
         }
-    })
+      })
       .then(res => {
-        props.history.push('/api/colors');
+        props.history.push("/api/colors");
       })
       .catch(error => {
         alert(error.response.data.error);
@@ -22,12 +22,12 @@ export default function AddColor(props) {
   };
 
   return (
-    <div className='login'>
-      <div className='login-inputs'>
-        color name: <input ref={colorNameRef} type="text" />
+      <div className="add-color-form">
+      <h1>Add Color:</h1>
+      <div className="login-inputs">
+        <input placeholder="color name" ref={colorNameRef} type="text" />
         <br />
-        color hex: <input ref={colorHexRef} type="text" />
-     
+        <input placeholder="color hex" ref={colorHexRef} type="text" />
       </div>
 
       <div>
